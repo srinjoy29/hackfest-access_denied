b9a199e9b764b7b9e80e8df0ce7c1e18d851f341
srinj <96923080+srinjoy29@users.noreply.github.com>
2024-05-11 04:14:09 +0530

    code Ide development

---
+++ code ide/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary bg-pattern py-4">
+      <div className="container mx-auto px-4">
+        <div className="flex flex-col items-center gap-y-2 justify-center">
+          
+          <div className="text-black font-medium text-sm">
+            &copy; 2024. All rights reserved.
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ code ide/src/components/OutputDetails.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const OutputDetails = ({ outputDetails }) => {
+    return (
+        <div className="metrics-container mt-4 flex flex-col space-y-3">
+            <p className="text-sm text-yellow-300">
+                Status:{" "}
+                <span className="font-semibold px-2 py-1 rounded-md bg-gray-100 text-black">
+                    {outputDetails?.status?.description}
+                </span>
+            </p>
+            <p className="text-sm text-yellow-300">
+                Memory:{" "}
+                <span className="font-semibold px-2 py-1 rounded-md bg-gray-100 text-black">
+                    {outputDetails?.memory}
+                </span>
+            </p>
+            <p className="text-sm text-yellow-300">
+                Time:{" "}
+                <span className="font-semibold px-2 py-1 rounded-md bg-gray-100 text-black">
+                    {outputDetails?.time}
+                </span>
+            </p>
+        </div>
+    );
+};
+export default OutputDetails;
