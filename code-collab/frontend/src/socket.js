2ec7e8335b82298c0824ba953dc2d21130590283
sumit <sumitkundu10012001@gmail.com>
2024-05-11 09:47:14 +0530

    add code collab frontend

---
+++ code-collab/frontend/src/socket.js
@@ -0,0 +1,12 @@
+import { io } from "socket.io-client";
+
+export const initSocket = async () => {
+  const options = {
+    "force new connection": true,
+    reconnectionAttempt: "Infinity",
+    timeout: 10000,
+    transports: ["websocket"],
+  };
+  const socket = io("https://collab-server-3.onrender.com/", options);
+  return socket;
+};
